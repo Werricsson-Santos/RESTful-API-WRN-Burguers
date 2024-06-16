ed3db79fecbfecae4f1dd94ed425b9ff6a7673c9
Werricsson <werricsson.santos@hotmail.com>
2024-06-16 15:37:26 -0300

    Creating Product Class

---
+++ src/main/java/dev/wericson/wrn_burguers/domain/model/Product.java
@@ -0,0 +1,46 @@
+package dev.wericson.wrn_burguers.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    
+    @Column(scale = 2, precision = 2)
+    private BigDecimal price;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
+
