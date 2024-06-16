f8383003a91613acb69fd534e80e3eacfde3a07c
Werricsson <werricsson.santos@hotmail.com>
2024-06-16 16:42:03 -0300

    Removing jakarta.criteria.Order import

--- src/main/java/dev/wericson/wrn_burguers/domain/model/Customer.java
+++ src/main/java/dev/wericson/wrn_burguers/domain/model/Customer.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.criteria.Order;
 
 @Entity
 public class Customer {
