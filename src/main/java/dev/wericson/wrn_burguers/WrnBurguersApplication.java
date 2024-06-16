9582fa6a8fefcc21876194fd59ca10da087505ed
Werricsson <werricsson.santos@hotmail.com>
2024-06-16 14:46:51 -0300

    Initial commit

---
+++ src/main/java/dev/wericson/wrn_burguers/WrnBurguersApplication.java
@@ -0,0 +1,13 @@
+package dev.wericson.wrn_burguers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrnBurguersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrnBurguersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wericson/wrn_burguers/WrnBurguersApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wericson.wrn_burguers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrnBurguersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
