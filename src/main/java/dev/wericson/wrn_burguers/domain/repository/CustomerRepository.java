3361fdf88e1db9333ac4ca9939a887f1aa15643c
Werricsson <werricsson.santos@hotmail.com>
2024-06-16 15:42:06 -0300

    Creating Customer Repository interface

---
+++ src/main/java/dev/wericson/wrn_burguers/domain/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package dev.wericson.wrn_burguers.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.wericson.wrn_burguers.domain.model.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
