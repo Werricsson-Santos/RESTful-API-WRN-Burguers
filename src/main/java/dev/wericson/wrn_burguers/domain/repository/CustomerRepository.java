e55c09827e1d009a36dad3cbfc0baba346d43392
Werricsson <werricsson.santos@hotmail.com>
2024-06-16 17:53:04 -0300

    Creating existsByCpf method

--- src/main/java/dev/wericson/wrn_burguers/domain/repository/CustomerRepository.java
+++ src/main/java/dev/wericson/wrn_burguers/domain/repository/CustomerRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import dev.wericson.wrn_burguers.domain.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	boolean existsByCpfNumber(String CPF);
 
 }
