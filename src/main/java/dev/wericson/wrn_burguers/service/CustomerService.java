3c92791adcbd72158b8cc89c553d3770e6c478c6
Werricsson <werricsson.santos@hotmail.com>
2024-06-18 10:45:13 -0300

    Creating convert to DTO method

--- src/main/java/dev/wericson/wrn_burguers/service/CustomerService.java
+++ src/main/java/dev/wericson/wrn_burguers/service/CustomerService.java
@@ -1,7 +1,10 @@
 package dev.wericson.wrn_burguers.service;
 
 import dev.wericson.wrn_burguers.domain.model.Customer;
+import dev.wericson.wrn_burguers.dto.CustomerDTO;
 
 public interface CustomerService extends CrudService<Long, Customer> {
+
+	public CustomerDTO convertToDTO(Customer customer);
 
 }
