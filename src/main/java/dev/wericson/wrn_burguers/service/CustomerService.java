3de9a156376a102000258d6061b7ba91185527ac
Werricsson <werricsson.santos@hotmail.com>
2024-06-18 20:01:29 -0300

    Removing trash

--- src/main/java/dev/wericson/wrn_burguers/service/CustomerService.java
+++ src/main/java/dev/wericson/wrn_burguers/service/CustomerService.java
@@ -1,10 +1,7 @@
 package dev.wericson.wrn_burguers.service;
 
 import dev.wericson.wrn_burguers.domain.model.Customer;
-import dev.wericson.wrn_burguers.dto.CustomerDTO;
 
 public interface CustomerService extends CrudService<Long, Customer> {
-
-	public CustomerDTO convertToDTO(Customer customer);
 
 }
