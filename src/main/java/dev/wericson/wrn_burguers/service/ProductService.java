89a5aaedb714f4920fab93c90691fab70916fa59
Werricsson <werricsson.santos@hotmail.com>
2024-06-18 10:51:48 -0300

    Removing trash

--- src/main/java/dev/wericson/wrn_burguers/service/ProductService.java
+++ src/main/java/dev/wericson/wrn_burguers/service/ProductService.java
@@ -1,10 +1,7 @@
 package dev.wericson.wrn_burguers.service;
 
-import org.springframework.stereotype.Service;
-
 import dev.wericson.wrn_burguers.domain.model.Product;
 
-@Service
 public interface ProductService extends CrudService<Long, Product> {
 
 }
