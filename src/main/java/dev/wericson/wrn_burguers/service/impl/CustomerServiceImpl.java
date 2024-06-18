fcdd3f83687cb353047490a68427f32b9c55a86b
Werricsson <werricsson.santos@hotmail.com>
2024-06-18 20:04:33 -0300

    Fixing methods to not expose customers CPF

--- src/main/java/dev/wericson/wrn_burguers/service/impl/CustomerServiceImpl.java
+++ src/main/java/dev/wericson/wrn_burguers/service/impl/CustomerServiceImpl.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import dev.wericson.wrn_burguers.domain.model.Customer;
+import dev.wericson.wrn_burguers.domain.model.Order;
 import dev.wericson.wrn_burguers.domain.repository.CustomerRepository;
-import dev.wericson.wrn_burguers.dto.CustomerDTO;
 import dev.wericson.wrn_burguers.service.CustomerService;
 import dev.wericson.wrn_burguers.service.exception.BusinessException;
 import dev.wericson.wrn_burguers.service.exception.NotFoundException;
@@ -38,7 +38,8 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	@Transactional
 	public Customer create(Customer customerToCreate) {
-		ofNullable(customerToCreate).orElseThrow(() -> new BusinessException("Customer to create must have an CPF."));
+		ofNullable(customerToCreate.getName()).orElseThrow(() -> new BusinessException("Customer to create must have a name."));
+		ofNullable(customerToCreate.getCPF()).orElseThrow(() -> new BusinessException("Customer to create must have an CPF."));
 
 		if(customerRepository.existsByCpf(customerToCreate.getCPF())) {
 			throw new BusinessException("This CPF number already exists. Customer to create must have an unique CPF.");
@@ -51,14 +52,27 @@ public class CustomerServiceImpl implements CustomerService {
 	@Transactional
 	public Customer update(Long id, Customer customerToUpdate) {
 		Customer dbCustomer = this.findById(id);
+		customerToUpdate.setId(id);
 		if (!dbCustomer.getId().equals(customerToUpdate.getId())) {
 			throw new BusinessException("Update IDs must be the same.");
 		}
 
-		dbCustomer.setName(customerToUpdate.getName());
-		dbCustomer.setCPF(customerToUpdate.getCPF());
+		if(customerToUpdate.getName() != null) 
+			dbCustomer.setName(customerToUpdate.getName());
+		if(customerToUpdate.getCPF() != null)
+			dbCustomer.setCPF(customerToUpdate.getCPF());
 		dbCustomer.setOrders(customerToUpdate.getOrders());
 		return this.customerRepository.save(dbCustomer);
+	}
+	
+	public Order makeOrder(Long id, Customer customerToMakeOrder) {
+		Customer dbCustomer = this.findById(id);
+		customerToMakeOrder.setId(id);
+		
+		if(customerToMakeOrder.getOrders() != null) {
+			Order newOrder = Order(customerToMakeOrder.getOrders().stream().findFirst().orElseThrow());
+			dbCustomer.setOrders(null);
+		}
 	}
 
 	@Override
@@ -66,16 +80,6 @@ public class CustomerServiceImpl implements CustomerService {
 	public void delete(Long id) {
 		Customer dbCustomer = this.findById(id);
 		this.customerRepository.delete(dbCustomer);
-	}
-
-	@Override
-	public CustomerDTO convertToDTO(Customer customerToConvert) {
-		CustomerDTO customerDTO = new CustomerDTO();
-		customerDTO.setId(customerToConvert.getId());
-		customerDTO.setName(customerToConvert.getName());
-		customerDTO.setOrders(customerToConvert.getOrders());
-
-		return customerDTO;
 	}
 
 }
