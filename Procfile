168704d3e8f5269ff9a82d65e9e6172baccd1a83
Werricsson <werricsson.santos@hotmail.com>
2024-06-19 15:50:08 -0300

    First attempt deploy on railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/wrn-burguers-0.0.1-SNAPSHOT.jar
--- src/main/java/dev/wericson/wrn_burguers/Application.java
+++ src/main/java/dev/wericson/wrn_burguers/Application.java
@@ -15,10 +15,10 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
-public class WrnBurguersApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WrnBurguersApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 	
 	@Bean
